3b8cc36d06a0612c3e270089aa1871a03ff9e1bf
Katherine <kmmpatke@gmail.com>
2025-11-10 16:57:49 -0600

    players with zero/1 loss, accepted

--- hashing/players_with_zero_or_one_losses.rb
+++ hashing/players_with_zero_or_one_losses.rb
@@ -13,9 +13,9 @@
 
 # Constraints:
 
-# 1 <= matches.length <= 105
+# 1 <= matches.length <= 10^5
 # matches[i].length == 2
-# 1 <= winner(i), loser(i) <= 105
+# 1 <= winner(i), loser(i) <= 10^5
 # winner(i) != loser(i)
 # All matches[i] are unique.
 
@@ -30,6 +30,30 @@
 
 # @param {Integer[][]} matches
 # @return {Integer[][]}
+# Runtime: O(n + n + (n * logn)) => O(n *logn)
+# Space: O(n)
 def find_winners(matches)
-    
+    losses = {}
+    matches.each do |match|
+        winner = match[0]
+        loser = match[1]
+        if losses[winner].nil?
+            losses[winner] = 0
+        end
+        if losses[loser].nil?
+            losses[loser] = 0
+        end
+        losses[winner] = losses[winner] + 0
+        losses[loser] = losses[loser] + 1
+    end
+    all_wins = []
+    one_loss = []
+    losses.keys.each do |player|
+        if losses[player] == 0
+            all_wins.push(player)
+        elsif losses[player] == 1
+            one_loss.push(player)
+        end
+    end
+    return [all_wins.sort, one_loss.sort]
 end
--- hashing/players_with_zero_or_one_losses_spec.rb
+++ hashing/players_with_zero_or_one_losses_spec.rb
@@ -1,15 +1,29 @@
-require_relative './counting_elements'
+require_relative './players_with_zero_or_one_losses'
 
 describe "#find_winners" do
     it "should return the correct output for provided example 1" do
         matches = [[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]
+
         result = find_winners(matches)
         expect(result).to eq([[1,2,10],[4,5,7,8]])
     end
 
-    it "should return the correct output for provided example 1" do
+    it "should return the correct output for provided example 2" do
         matches = [[2,3],[1,3],[5,4],[6,4]]
         result = find_winners(matches)
         expect(result).to eq([[1,2,5,6],[]])
+    end
+
+    it "should return empty array for lossless players if everyone lost at least once" do
+        matches = [[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9],[9,1],[9,2],[6,10]]
+        # {1: 1, 2: 1, 3: 2, 4: 1, 5: 1, 6: 2, 7: 1, 8: 1, 9: 2, 10: 1}
+        result = find_winners(matches)
+        expect(result).to eq([[],[1,2,4,5,7,8,10]])
+    end
+
+    it "should return the correct output for provided example 2" do
+        matches = [[200,300]]
+        result = find_winners(matches)
+        expect(result).to eq([[200],[300]])
     end
 end
