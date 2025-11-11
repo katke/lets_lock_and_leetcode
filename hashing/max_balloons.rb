6a801f0779ed093bbe9054e09e860a048b225c62
Katherine <kmmpatke@gmail.com>
2025-11-11 14:37:53 -0600

    max num of balloons, accepted

--- hashing/max_balloons.rb
+++ hashing/max_balloons.rb
@@ -21,35 +21,44 @@
 
 # @param {String} text
 # @return {Integer}
+# Runtime: O(7 + n + 26 + 5) => O(38 + 2n) => O(n) 
+# Space: O(26 + 5) => O(1) 
 def max_number_of_balloons(text)
-    # create char freq map
-    balloon_freq = {b: 1, a: 1, l: 2, o: 2, n: 1}
-    supports = {}
-    char_freq = {}
-    text.chars.each do |c|
-        if char_freq[c].nil?
-            char_freq[c] = 1
-        else
-            char_freq[c] = char_freq[c] + 1
-        end
-    end
+    # create char freq map from input text
+    balloon_freq = create_char_freq("balloon")
+    max_char_avail = {}
+    input_freq = create_char_freq(text)
     # iterate char by char and check each char freq to see how many times each one could support balloon
-    char_freq.keys.each do |c|
+    input_freq.keys.each do |c|
+        # we only care about chars present in balloon, and if the string has at least the min number of chars to complete the word at least once
         if balloon_freq[c] 
-            max = char_freq[c] / balloon_freq[c]
-            supports[c] = max
+            if input_freq[c] < balloon_freq[c]
+                return 0
+            end
+            max = input_freq[c] / balloon_freq[c]
+            max_char_avail[c] = max
         end
     end
     # make sure we actually have all the characters we need
-    balloon_freq.keys.each do |k|
-        if char_freq[k].nil?
-            return 0
+    unless max_char_avail.keys.length == 5
+        return 0
+    end
+    min_times = 10 ** 4
+    # the min num for b, a, l, o, and n will tell us the max number of times we could find the full word, i.e. even if we have enough b's, a's, l's, and o's for five appearances, if we only have enough n's for once, then we can only find the word one time
+    max_char_avail.keys.each do |c|
+        min_times = max_char_avail[c] < min_times ? max_char_avail[c] : min_times
+    end
+    min_times
+end
+
+def create_char_freq(text)
+    input_freq = {}
+    text.chars.each do |c|
+        if input_freq[c].nil?
+            input_freq[c] = 1
+        else
+            input_freq[c] = input_freq[c] + 1
         end
     end
-    max_times = 10 ** 4
-    # the min num for b, a, l, o, and n will tell us the max number of times we could find it
-    supports.each do |c|
-        max_times = supports[c] < max_times ? supports[c] : max_times
-    end
-    max_times
+    input_freq
 end
--- hashing/max_balloons_spec.rb
+++ hashing/max_balloons_spec.rb
@@ -6,4 +6,28 @@ describe "#max_number_of_balloons" do
         result = max_number_of_balloons(text)
         expect(result).to eq(1)
     end
+
+    it "should return the correct output for provided example 2" do
+        text = "loonbalxballpoon"
+        result = max_number_of_balloons(text)
+        expect(result).to eq(2)
+    end
+
+    it "should return the correct output for provided example 3" do
+        text = "leetcode"
+        result = max_number_of_balloons(text)
+        expect(result).to eq(0)
+    end
+
+    it "only counts a full balloon when every char in the word has sufficient available repeats" do
+        text = "loonbalxballpoonbaloon" # 1 L short of a 3rd appearance
+        result = max_number_of_balloons(text)
+        expect(result).to eq(2)
+    end
+
+    it "does not count it when one letter is missing" do
+        text = "balloo"
+        result = max_number_of_balloons(text)
+        expect(result).to eq(0)
+    end
 end
