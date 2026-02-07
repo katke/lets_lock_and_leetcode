eb0857ffc49b90ee0c1c8501c9414d6a0ec1b60d
Katherine <kmmpatke@gmail.com>
2026-02-07 14:06:30 -0600

    cleanup

--- backtracking/word_search.rb
+++ backtracking/word_search.rb
@@ -4,8 +4,8 @@
 # Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
 # Output: true
 
-@board = "not initalized"
-@word = "not initalized"
+@board = "not initialized"
+@word = "not initialized"
 
 # @param {Character[][]} board
 # @param {String} word
@@ -16,6 +16,11 @@ def word_exists(board, word)
   end
   @board = board
   @word = word
+  puts "target word: #{@word}"
+  puts "BOARD:"
+  @board.each do |row|
+    puts "#{row}"
+  end
   @board.each_with_index do |row, i|
     row.each_with_index do |_, j|
         found_word = backtrack(0, i, j)
@@ -28,27 +33,30 @@ def word_exists(board, word)
 end
 
 def backtrack(target_idx, row, col)
-  unless is_valid_coord([row, col], @board.length, @board[0].length)
+  puts "-------"
+  puts "backtrack(), current target index: #{target_idx} (looking for #{@word[target_idx]}. row: #{row} col: #{col}"
+  unless is_valid_coord(row, col)
+    puts "coordinate not valid"
     return false
   end
   letter = @board[row][col]
   if  letter != @word[target_idx]
+    puts "dead end for letter #{letter}"
     return false # dead end
   end
   if target_idx == @word.length - 1
+    puts "we've reached the end of the target word, huzzah"
     return true
   end
   @board[row][col] = "%"
   found = backtrack(target_idx + 1, row + 1, col) ||
-         backtrack(target_idx + 1, row - 1, col) ||
-         backtrack(target_idx + 1, row, col + 1) ||
-         backtrack(target_idx + 1, row, col - 1)
+          backtrack(target_idx + 1, row, col + 1) ||
+          backtrack(target_idx + 1, row - 1, col) ||
+          backtrack(target_idx + 1, row, col - 1)
   @board[row][col] = letter
   found
 end
 
-def is_valid_coord(coords, max_rows, max_cols)
-  row = coords[0]
-  col = coords[1]
-  row >= 0 && col >= 0 && row < max_rows && col < max_cols
+def is_valid_coord(row, col)
+  row >= 0 && col >= 0 && row < @board.length && col < @board[0].length
 end
--- backtracking/word_search_spec.rb
+++ backtracking/word_search_spec.rb
@@ -1,34 +1,36 @@
 require_relative "./word_search"
 
 describe "#word_exists" do
-  board = [%w[A B C E], %w[S F C S], %w[A D E E]]
   it "returns expected result for provided ex 1" do
+    board = [%w[A B C E], %w[S F C S], %w[A D E E]]
     word = "ABCCED"
     result = word_exists(board, word)
     expect(result).to be_truthy
   end
 
   it "returns expected result for provided ex 2" do
+    board = [%w[A B C E], %w[S F C S], %w[A D E E]]
     word = "SEE"
     result = word_exists(board, word)
     expect(result).to be_truthy
   end
 
   it "returns expected result for provided ex 3" do
+    board = [%w[A B C E], %w[S F C S], %w[A D E E]]
     word = "ABCB"
     result = word_exists(board, word)
     expect(result).to be_falsey
   end
 
   it "passes failing test case" do
-    board = [["C","A","A"],["A","A","A"],["B","C","D"]]
+    board = [%w[C A A], %w[A A A], %w[B C D]]
     word = "AAB"
     result = word_exists(board, word)
     expect(result).to be_truthy
   end
 
   it "passes other failing test case" do
-    board = [["A","B","C","E"],["S","F","E","S"],["A","D","E","E"]]
+    board = [%w[A B C E], %w[S F E S], %w[A D E E]]
     word = "ABCEFSADEESE"
     result = word_exists(board, word)
     expect(result).to be_truthy
